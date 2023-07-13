a16108359e955b0f8d4146db4fb7e7a775f5225f
vasyl.pashko <vasyl.pashko@gmail.com>
2023-07-13 08:38:59 +0300

    adding modal create email

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -9,7 +9,8 @@ import { EmailIndexComponent } from './email-index/email-index.component';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { SharedModule } from '../shared/shared.module';
+import { EmailFormComponent } from './email-form/email-form.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +20,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
     EmailIndexComponent,
     EmailShowComponent,
     PlaceholderComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    EmailFormComponent,
   ],
-  imports: [
-    CommonModule,
-    InboxRoutingModule
-  ]
+  imports: [CommonModule, InboxRoutingModule, SharedModule],
 })
-export class InboxModule { }
+export class InboxModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { InputComponent } from './input/input.component';
+import { ModalComponent } from './modal/modal.component';
+
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, ModalComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [InputComponent],
+  exports: [InputComponent, ModalComponent],
 })
 export class SharedModule {}
